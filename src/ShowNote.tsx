3be8bad1bb95d5b711208c1e8a6c6717da415a1d
milutinovici <milutinovici@gmail.com>
2020-12-20 17:40:05 +0100

    Better handling keyup and keydown

--- src/ShowNote.tsx
+++ src/ShowNote.tsx
@@ -10,23 +10,26 @@ import React from 'react';
 export class ShowNote extends React.Component<MyProps, MyState> {
   constructor(props: string){
     super(props);
-    this.state = {keys: [""]};
+    this.state = {keys: []};
   }
   handleKeyDown = (key: string) => {
-    this.state.keys.push(key);
-    this.setState({keys: this.state.keys});
+    let alreadyExists = this.state.keys.some(x => x === key);
+    if(!alreadyExists) {
+      this.setState({keys: [...this.state.keys, key]});
+    };
+    
   }
-  handleKeyUp = () => {
-    this.state.keys.pop();
-    this.setState({keys: this.state.keys});
+  handleKeyUp = (key: string) => {
+    let index = this.state.keys.findIndex(x => x === key);
+    this.setState({keys: [...this.state.keys.slice(0, index), ...this.state.keys.slice(index + 1,this.state.keys.length)]});
   }
   componentDidMount() {
     document.addEventListener("keydown",(e: KeyboardEvent) => this.handleKeyDown(e.key));
-    document.addEventListener("keyup",this.handleKeyUp);
+    document.addEventListener("keyup",(e: KeyboardEvent) => this.handleKeyUp(e.key));
   }
   componentWillUnmount() {
     document.removeEventListener("keydown",(e: KeyboardEvent) => this.handleKeyDown(e.key));
-    document.removeEventListener("keyup",this.handleKeyUp);
+    document.removeEventListener("keyup",(e: KeyboardEvent) => this.handleKeyUp(e.key));
   }
   render() {
     return <div>{this.state.keys}</div>;
